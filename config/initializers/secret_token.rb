94e3b048b5dd2de3459361928d12d2592c8704d5
Sridhar Madduluri <sridhar2@msn.com>
2012-02-25 23:56:46 -0500

    Contacts Rail App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = '4b3f265df719e9db8980dc052efd668955c48552a75cadf81672619b5596c31a560bf0a4bbff5925f76315f5ef73391572a8757fe54663b05a09a240a5406518'
